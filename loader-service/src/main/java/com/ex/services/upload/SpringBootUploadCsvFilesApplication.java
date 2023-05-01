a6a590a988419c68a2285a25adf0e0a883639a98
Vladimir_Shmalko <Vladimir_Shmalko@epam.com>
2023-05-01 17:57:26 +0200

    OpenApi

--- loader-service/src/main/java/com/ex/services/upload/SpringBootUploadCsvFilesApplication.java
+++ loader-service/src/main/java/com/ex/services/upload/SpringBootUploadCsvFilesApplication.java
@@ -1,11 +1,16 @@
 package com.ex.services.upload;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
+@OpenAPIDefinition(info =
+@Info(title = "Loader API", version = "1.0", description = "Documentation Loader CSV API v1.0")
+)
 public class SpringBootUploadCsvFilesApplication {
 
 	public static void main(String[] args) {
--- loader-service/src/main/java/com/ex/services/upload/model/stock/AaplStockHistory.java
+++ loader-service/src/main/java/com/ex/services/upload/model/stock/AaplStockHistory.java
@@ -3,9 +3,11 @@ package com.ex.services.upload.model.stock;
 import com.ex.services.upload.annotation.StockName;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 
 @Entity
 @StockName("AAPL")
+@AllArgsConstructor
 @Table(name = "apple_stock_history")
 public class AaplStockHistory extends StockHistory {
 }
